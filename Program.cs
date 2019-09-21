755060267f4ee6cb76239018f1a8c7573d49362e
Александр Трошкин <aleksandr.troshkin@outlook.com>
2019-09-21 21:30:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = LastDigit(new int[] { 0, 0, 0 });
+                Console.WriteLine(result);
+
+        }
+
+        public static int LastDigit(int[] array)
+        {
+            if (array.Length == 0)
+                return 1;
+
+            var digits = new int[array.Length];
+            for (var i = 0; i < digits.Length; i++)
+                digits[i] = array[i] % 10;
+
+            double pow = digits[0];
+            // TODO: дожна работать с последнего числа
+            for (var i = 0; i < digits.Length - 1; i++)
+                pow = Math.Pow(pow, digits[i + 1]);
+
+            return (int)(pow % 10);
+        }
+    }
+}
