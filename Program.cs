002f3c2005b41f7f069232bb5d223baccb3b9ecc
Aleksandr Troshkin <aleksandr.troshkin@outlook.com>
2019-09-24 22:36:32 +0300

    перед уможением digits на 10

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace CodeWars
     {
         static void Main(string[] args)
         {
-            var result = LastDigit(new int[] { 0, 0, 0 });
+            var result = LastDigit(new int[] { 7, 6, 21 });
                 Console.WriteLine(result);
 
         }
@@ -20,16 +20,18 @@ namespace CodeWars
         {
             if (array.Length == 0)
                 return 1;
+            if (array.Length == 1)
+                return array[0];
 
             var digits = new int[array.Length];
             for (var i = 0; i < digits.Length; i++)
-                digits[i] = array[i] % 10;
-            double pow = digits[0];
-            // TODO: дожна работать с последнего числа
-            for (var i = 0; i < digits.Length - 1; i++)
-                pow = Math.Pow(pow, digits[i + 1]);
+                digits[i] = array[i] % 100;
 
-            return (int)(pow % 10);
+            double pow = digits[digits.Length - 1];
+            for (var i = digits.Length - 2; i >= 0; i--)
+                pow = (Math.Pow(digits[i], pow)) % 10;
+
+            return (int)pow;
         }
     }
 }
